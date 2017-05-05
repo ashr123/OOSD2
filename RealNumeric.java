cec334045c8b936fb459035f900cc3afffb0e333
Saarule <saaryazdi@gmail.com>
2017-05-05 15:27:16 +0300

    RealNumeric2

--- RealNumeric.java
+++ RealNumeric.java
@@ -7,19 +7,6 @@ public class RealNumeric implements Numeric
 		this.num=num;
 	}
 	
-<<<<<<< Updated upstream
-=======
-	public double getNum()
-	{
-		return num;
-	}
-	
-	public void setNum(double num)
-	{
-		this.num=num;
-	}
-	
->>>>>>> Stashed changes
 	@Override
 	public Numeric transformAdd(Numeric n)
 	{
@@ -41,11 +28,9 @@ public class RealNumeric implements Numeric
 	@Override
 	public boolean equals(Object obj)
 	{
-		if (obj instanceof RealNumeric)
-			return num == ((RealNumeric)obj).getNum();
-		if (obj instanceof RationalNumeric)
-			return num == obj.g
-		return super.equals(obj);
+		if (obj instanceof Numeric)
+			return obj.equals(this);
+		return false;
 	}
 	
 	public double getNum()
